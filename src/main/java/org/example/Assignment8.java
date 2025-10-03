2c2bf756c0896e5a60e0f11a690a365d4796e2d7
sharmadotexe <rockvikas300@gmail.com>
2025-10-04 00:37:00 +0530

    Assignment 8

---
+++ src/main/java/org/example/Assignment8.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+public class Assignment8 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        driver.findElement(By.id("autocomplete")).sendKeys("mala");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+        driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+        driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+
+        System.out.println(driver.findElement(By.id("autocomplete")).getAttribute("value"));
+        }
+    }
+
